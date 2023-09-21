540e2bddce3483929d7efc4c1a2d44efe3d7c171
KarlJug <98745340+KarlJug@users.noreply.github.com>
2023-09-21 11:37:42 +0300

    Add files via upload

---
+++ Ülesanne02/ylesnne02.js
@@ -0,0 +1,12 @@
+/**
+ * Karl Jugapuu
+ * Üleasnne 2
+ * 21.09.2023
+ */
+
+// aeg
+const tunnid = 2;
+const minutid = 38;
+const sekundid = 50;
+const aeg = tunnid + ':' + minutid + ':' + sekundid
+console.log(aeg)
